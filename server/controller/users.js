c0b2fcc30ad5675a15fcc742cd6bbbc03b5755d4
sidhdhali <sidhnakrani1911@gmail.com>
2024-06-21 16:49:50 +0200

    able to register

--- server/controller/users.js
+++ server/controller/users.js
@@ -24,3 +24,44 @@ export const register = asyncHandler(async (req, res, next) =>
   const token = jwt.sign({ uid: newUser._id }, process.env.JWT_SECRET);
   res.status(201).send({ token });
 });
+
+// LOGIN
+export const login = asyncHandler(async (req, res, next) =>
+{
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email }).select('+password');
+  if (!existingUser) throw new ErrorResponse('Email does not exist', 404);
+
+  const match = await bcrypt.compare(password, existingUser.password);
+  if (!match) throw new ErrorResponse('Password is incorrect', 401);
+
+  const token = jwt.sign({ uid: existingUser._id }, process.env.JWT_SECRET, {
+    expiresIn: '30m',
+  });
+  // res.json({ token });
+  res.cookie('token', token, { maxAge: 1800000 }); // 30mn
+  res.send({ status: 'success' });
+});
+
+// Verify User
+export const getUser = asyncHandler(async (req, res, next) =>
+{
+  const user = await User.findById(req.uid);
+  res.json(user);
+});
+
+// Logout....
+export const logout = asyncHandler(async (req, res, next) =>
+{
+  res.clearCookie('token');
+  res.send({ status: 'success' });
+});
+
+
+//getAllUsers
+export const getAllUsers = asyncHandler(async (req, res, next) =>
+{
+  const users = await User.find();
+  res.json(users);
+});
--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -5,6 +5,11 @@ import verifyToken from '../middleware/verifyToken.js';
 const usersRouter = Router();
 usersRouter.post('/register', userController.register);
 usersRouter.post('/login', userController.login);
+usersRouter.get('/me', verifyToken, userController.getUser);
+
+usersRouter.get('/', userController.getAllUsers);
+usersRouter.post('/logout', verifyToken, userController.logout);
+
 
 
 export default usersRouter;
