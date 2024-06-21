42bebf952a9de34973a24bed6bd3bf40515cc908
Hanno Bielz <bielzhanno@gmail.com>
2024-06-21 10:52:24 +0200

    homefix

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home</title>
-</head>
-<body>
-    <h1>lalala</h1>
-</body>
-</html>
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
