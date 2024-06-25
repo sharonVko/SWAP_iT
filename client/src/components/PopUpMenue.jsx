66c0014d1cfb5b3bfaabb1f6485eca8899b442a0
SGitHub2023 <sabrina.i.gerhardt@gmail.com>
2024-06-25 15:11:22 +0200

    folder structure and routing frontend

--- client/src/components/PopUpMenue.jsx
+++ client/src/components/PopUpMenue.jsx
@@ -7,26 +7,22 @@ const PopUpMenue = () => {
 	return (
 		<div>
 			<div className="text-teal-700 text-4xl">PopUpMenue</div>
-
 			<Link className="text-cyan-600" to="/">
 				Home...
 			</Link>
-
 			<Link className="text-cyan-600" to="/chats">
 				Chats...
 			</Link>
-
 			<Link className="text-cyan-600" to="/profile">
 				Account...
 			</Link>
-
 			<Link className="text-cyan-600" to="/createad">
 				Create Ad..
 			</Link>
-
 			<Link className="text-cyan-600" to="/logout">
 				Logout...
 			</Link>
+			Favouriten
 		</div>
 	);
 };
--- client/src/views/SingleViewAd.jsx
+++ client/src/views/SingleViewAd.jsx
@@ -6,10 +6,10 @@ const SingleViewAd = () => {
 	return (
 		<div>
 			<div className="text-teal-700 text-4xl">Produkt</div>
-			<Link className="text-cyan-600 text-2xl" to="/chats">
+			<Link className="text-cyan-600 text-2xl" to="/singlechat">
 				Kontakt Aufnehmen...
 			</Link>
-			<Link className="text-cyan-600 text-2xl" to="popupmenue">
+			<Link className="text-cyan-600 text-2xl" to="/popupmenue">
 				PopUpMenue...
 			</Link>
 			Leaflet..Category..Tags
