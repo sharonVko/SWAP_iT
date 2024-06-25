5df5a311403f488a51ba6f7c4513268e5736806c
Hanno Bielz <bielzhanno@gmail.com>
2024-06-25 11:45:11 +0200

    updated logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,74 @@
-import {categories} from "./utils/categories.js";
-import {users} from "./utils/users.js";
-import {ads} from "./utils/ads.js";
+import { categories } from "./utils/categories.js";
+import { users } from "./utils/users.js";
+import { ads } from "./utils/ads.js";
 
 function App() {
+  const getCategories = (id = 0) => {
+    return categories.filter((category) => category.parent === id);
+  };
 
-	const getCategories = (id = 0) => {
-		return categories.filter(category => {
-			if (category.parent === id) {
-				return category;
-			}
-		})
-	}
-	//const maincats = getCategories();
+  const getUserById = (id) => {
+    return users.find((user) => user.user_id === id);
+  };
 
-	const getUserById = (id) => {
-		return users.find((user) => user.user_id === id);
-	}
+  // Funktion, um die passenden Anzeigen für einen Benutzer zu erhalten
+  const getMatchingAds = (user) => {
+    return ads.filter((ad) => {
+      const adOwner = getUserById(ad.user_id);
+      return (
+        user.pref_cats === ad.ad_cat &&
+        adOwner.pref_cats ===
+          ads.find((a) => a.user_id === user.user_id)?.ad_cat
+      );
+    });
+  };
 
-	const myads = ads.map(ad => {
+  // Liste der Benutzer mit ihren passenden Anzeigen
+  const myUsers = users.map((user) => {
+    let matchedAds = getMatchingAds(user);
+    let ownAd = ads.find((ad) => ad.user_id === user.user_id);
 
-		let adId = ad.ad_id;
-		let adUserid = ad.user_id;
-		let adUserName = getUserById(ad.user_id).name;
-		let adCat = ad.ad_cat;
+    return {
+      user_id: user.user_id,
+      name: user.name,
+      pref_cats: user.pref_cats,
+      ownAd: ownAd, // Hier wird die eigene Anzeige gespeichert
+      matchedAds: matchedAds, // Hier werden die passenden Anzeigen gespeichert
+    };
+  });
 
-		let interestedUsers = users.filter(user => {
-			return user.user_cats === adCat;
-		});
+  // Liste der Anzeigen mit den Benutzern, die daran interessiert sind
+  const myAds = ads.map((ad) => {
+    let adId = ad.ad_id;
+    let adUserId = ad.user_id;
+    let adUserName = getUserById(ad.user_id).name;
+    let adCat = ad.ad_cat;
 
-		return {
-			adId: adId,
-			adCat: adCat,
-			adUserid: adUserid,
-			adUserName: adUserName,
-			interestedUsers: interestedUsers
-		}
+    let interestedUsers = users.filter((user) => {
+      return (
+        user.pref_cats === adCat &&
+        ads.find((a) => a.user_id === user.user_id)?.ad_cat === adCat
+      );
+    });
 
-	});
+    return {
+      adId: adId,
+      adCat: adCat,
+      adUserId: adUserId,
+      adUserName: adUserName,
+      interestedUsers: interestedUsers, // Hier werden die interessierten Benutzer gespeichert
+    };
+  });
 
+  console.log(myAds); // Anzeigen mit interessierten Benutzern
+  console.log(myUsers); // Benutzer mit passenden Anzeigen
 
-
-	console.log(myads);
-	//console.log(users);
-
-	return (
+  return (
     <>
       <h1>Alle Ads</h1>
-			{ads.map((ad, i) => (<p key={i}>{ad.ad_id}</p>))}
+      {ads.map((ad, i) => (
+        <p key={i}>{ad.ad_id}</p>
+      ))}
     </>
   );
 }
--- client/src/utils/users.js
+++ client/src/utils/users.js
@@ -1,27 +1,27 @@
 export const users = [
-	{
-		user_id: 1001,
-		name: "Thomas",
-		user_cats: 1,
-	},
-	{
-		user_id: 1002,
-		name: "Hanno",
-		user_cats: 16,
-	},
-	{
-		user_id: 1003,
-		name: "Sharon",
-		user_cats: 16,
-	},
-	{
-		user_id: 1004,
-		name: "Sabrina",
-		user_cats: 60,
-	},
-	{
-		user_id: 1005,
-		name: "Sidhdhali",
-		user_cats: 60,
-	},
+  {
+    user_id: 1001,
+    name: "Thomas",
+    pref_cats: 1,
+  },
+  {
+    user_id: 1002,
+    name: "Hanno",
+    pref_cats: 16,
+  },
+  {
+    user_id: 1003,
+    name: "Sharon",
+    pref_cats: 1,
+  },
+  {
+    user_id: 1004,
+    name: "Sabrina",
+    pref_cats: 60,
+  },
+  {
+    user_id: 1005,
+    name: "Sidhdhali",
+    pref_cats: 60,
+  },
 ];
