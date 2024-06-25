34fb74dfa264f5035f9abb2fe52cbb61843da608
SGitHub2023 <sabrina.i.gerhardt@gmail.com>
2024-06-25 15:35:56 +0200

    minorchange

--- client/src/views/Chats.jsx
+++ client/src/views/Chats.jsx
@@ -8,11 +8,11 @@ const Chats = () => {
 		<div>
 			<div className="text-teal-700 text-4xl">Chats</div>
 
-			<Link className="text-cyan-600" to="/singlechat">
+			<Link className="text-cyan-600 text-2xl" to="/singlechat">
 				Einzelner Chat...
 			</Link>
 
-			<Link className="text-cyan-600" to="/popupmenue">
+			<Link className="text-cyan-600 text-2xl" to="/popupmenue">
 				PopUpMenue...
 			</Link>
 		</div>
