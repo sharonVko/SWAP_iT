78a96bbdc34e4210cbc8524184b26521f88f4425
tdo leipzig <tdo.leipzig@gmail.com>
2024-07-03 10:44:19 +0200

    debug create ad

--- client/src/views/CreateAd.jsx
+++ client/src/views/CreateAd.jsx
@@ -1,95 +1,101 @@
-import { useState } from 'react';
-import { useAuth } from "../context/AuthProvider.jsx";
-import LoginForm from "../components/LoginForm.jsx";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import { useAuth } from '../context/AuthProvider.jsx';
+import LoginForm from '../components/LoginForm.jsx';
+import axios from 'axios';
 
 const CreateAd = () => {
-
 	const { isLoggedIn, userData } = useAuth();
 
-		const [formData, setFormData] = useState({
-			user_id: userData._id,
-			title: "",
-			description: "",
-			tradeOption: false,
-			pickupaddress: {
-				street: "",
-				housenumber: "",
-				zip: "",
-				city: "",
-				country: "",
-			},
-			categories: "",
-			subCategory: "",
-			tags: "",
-		});
+	const [formData, setFormData] = useState({
+		user_id: '',
+		title: '',
+		description: '',
+		tradeOption: false,
+		pickupaddress: {
+			street: '',
+			housenumber: '',
+			zip: '',
+			city: '',
+			country: '',
+		},
+		categories: '',
+		subCategory: '',
+		tags: '',
+	});
 
-		const handleChange = (e) => {
-			const { name, value } = e.target;
-			if (name.includes("pickupaddress")) {
-				const [_, addressField] = name.split(".");
-				setFormData((prev) => ({
-					...prev,
-					pickupaddress: {
-						...prev.pickupaddress,
-						[addressField]: value,
-					},
-				}));
-			} else {
-				setFormData({ ...formData, [name]: value });
-			}
-		};
-		const handleCheckboxChange = (e) => {
-			setFormData({ ...formData, tradeOption: e.target.checked });
-		};
-		const handleSubmit = async (e) => {
-			e.preventDefault();
-			try {
-				const response = await axios.post(
-					"http://localhost:8000/ads/createAd",
-					formData
-				);
-				console.log("Ad created successfully:", response.data);
-				// Reset form
-				setFormData({
-					user_id: "",
-					title: "",
-					description: "",
-					tradeOption: false,
-					pickupaddress: {
-						street: "",
-						housenumber: "",
-						zip: "",
-						city: "",
-						country: "",
-					},
-					categories: "",
-					subCategory: "",
-					tags: "",
-				});
-			} catch (error) {
-				console.error("Error creating ad:", error);
-			}
-		};
+	useEffect(() => {
+		if (userData && userData._id) {
+			setFormData((prev) => ({
+				...prev,
+				user_id: userData._id,
+			}));
+		}
+	}, [userData]);
 
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		if (name.includes('pickupaddress')) {
+			const [_, addressField] = name.split('.');
+			setFormData((prev) => ({
+				...prev,
+				pickupaddress: {
+					...prev.pickupaddress,
+					[addressField]: value,
+				},
+			}));
+		} else {
+			setFormData({ ...formData, [name]: value });
+		}
+	};
 
-		console.log(userData._id);
+	const handleCheckboxChange = (e) => {
+		setFormData({ ...formData, tradeOption: e.target.checked });
+	};
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post(
+				'http://localhost:8000/ads/createAd',
+				formData,
+				{ withCredentials: true }
+			);
+			console.log('Ad created successfully:', response.data);
+			// Reset form
+			setFormData({
+				user_id: '',
+				title: '',
+				description: '',
+				tradeOption: Boolean,
+				pickupaddress: {
+					street: '',
+					housenumber: '',
+					zip: '',
+					city: '',
+					country: '',
+				},
+				categories: '',
+				subCategory: '',
+				tags: '',
+			});
+		} catch (error) {
+			console.error('Error creating ad:', error);
+		}
+	};
 
 	return (
 		<div>
-
 			{isLoggedIn ? (
 				<div>
 					<h1>Anzeige erstellen</h1>
 
-					<div className="create-ad-form-container">
-						<form className="create-ad-form" onSubmit={handleSubmit}>
+					<div className='create-ad-form-container'>
+						<form className='create-ad-form' onSubmit={handleSubmit}>
 							<div>
 								<label>Title:</label>
 								<input
-									type="text"
-									name="title"
+									type='text'
+									name='title'
 									value={formData.title}
 									onChange={handleChange}
 									required
@@ -98,7 +104,7 @@ const CreateAd = () => {
 							<div>
 								<label>Description:</label>
 								<textarea
-									name="description"
+									name='description'
 									value={formData.description}
 									onChange={handleChange}
 									required
@@ -107,8 +113,8 @@ const CreateAd = () => {
 							<div>
 								<label>Trade Option:</label>
 								<input
-									type="checkbox"
-									name="tradeOption"
+									type='checkbox'
+									name='tradeOption'
 									checked={formData.tradeOption}
 									onChange={handleCheckboxChange}
 								/>
@@ -116,8 +122,8 @@ const CreateAd = () => {
 							<div>
 								<label>Street:</label>
 								<input
-									type="text"
-									name="pickupaddress.street"
+									type='text'
+									name='pickupaddress.street'
 									value={formData.pickupaddress.street}
 									onChange={handleChange}
 								/>
@@ -125,8 +131,8 @@ const CreateAd = () => {
 							<div>
 								<label>House Number:</label>
 								<input
-									type="text"
-									name="pickupaddress.housenumber"
+									type='text'
+									name='pickupaddress.housenumber'
 									value={formData.pickupaddress.housenumber}
 									onChange={handleChange}
 								/>
@@ -134,8 +140,8 @@ const CreateAd = () => {
 							<div>
 								<label>Zip:</label>
 								<input
-									type="text"
-									name="pickupaddress.zip"
+									type='text'
+									name='pickupaddress.zip'
 									value={formData.pickupaddress.zip}
 									onChange={handleChange}
 								/>
@@ -143,8 +149,8 @@ const CreateAd = () => {
 							<div>
 								<label>City:</label>
 								<input
-									type="text"
-									name="pickupaddress.city"
+									type='text'
+									name='pickupaddress.city'
 									value={formData.pickupaddress.city}
 									onChange={handleChange}
 								/>
@@ -152,8 +158,8 @@ const CreateAd = () => {
 							<div>
 								<label>Country:</label>
 								<input
-									type="text"
-									name="pickupaddress.country"
+									type='text'
+									name='pickupaddress.country'
 									value={formData.pickupaddress.country}
 									onChange={handleChange}
 								/>
@@ -161,8 +167,8 @@ const CreateAd = () => {
 							<div>
 								<label>Categories:</label>
 								<input
-									type="text"
-									name="categories"
+									type='text'
+									name='categories'
 									value={formData.categories}
 									onChange={handleChange}
 									required
@@ -171,8 +177,8 @@ const CreateAd = () => {
 							<div>
 								<label>SubCategory:</label>
 								<input
-									type="text"
-									name="subCategory"
+									type='text'
+									name='subCategory'
 									value={formData.subCategory}
 									onChange={handleChange}
 									required
@@ -181,23 +187,20 @@ const CreateAd = () => {
 							<div>
 								<label>Tags:</label>
 								<input
-									type="text"
-									name="tags"
+									type='text'
+									name='tags'
 									value={formData.tags}
 									onChange={handleChange}
 									required
 								/>
 							</div>
-							<button type="submit">Create Ad</button>
+							<button type='submit'>Create Ad</button>
 						</form>
 					</div>
-
-
 				</div>
 			) : (
-				<LoginForm target="/profile/ads/new"/>
+				<LoginForm target='/profile/ads/new' />
 			)}
-
 		</div>
 	);
 };
