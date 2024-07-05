ec6d9effd0b494f3afa9835940b5569cb46de67f
unknown <onureredo@yahoo.com>
2024-07-05 18:09:12 +0200

    ad_id

--- client/src/views/SingleViewAd.jsx
+++ client/src/views/SingleViewAd.jsx
@@ -1,81 +1,85 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthProvider';
 import { useNavigate } from 'react-router-dom';
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const SingleViewAd = () => {
-	const { isLoggedIn, userData } = useAuth();
-	const [article, setArticle] = useState(null);
-	const [chatData, setChatData] = useState([]);
-	const { articleId } = useParams();
-	const navigate = useNavigate();
+  const { isLoggedIn, userData } = useAuth();
+  const [article, setArticle] = useState(null);
+  const [chatData, setChatData] = useState([]);
+  const { articleId } = useParams();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const fetchArticle = async () => {
-			try {
-				const response = await axios.get(`http://localhost:8000/ads/${articleId}`);
-				setArticle(response.data)
-			}
-			catch (error) {
-				console.error('Error fetching article:', error);
-			}
-		}
-		const fetchChats = async () => {
-			try {
-				const response = await axios.get('http://localhost:8000/chats/', {
-					withCredentials: true,
-				});
-				setChatData(response.data);
-			}
-			catch (error) {
-				console.error('Error fetching chats:', error);
-			}
-		};
+  useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/ads/${articleId}`
+        );
+        setArticle(response.data);
+      } catch (error) {
+        console.error('Error fetching article:', error);
+      }
+    };
+    const fetchChats = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/chats/', {
+          withCredentials: true,
+        });
+        setChatData(response.data);
+      } catch (error) {
+        console.error('Error fetching chats:', error);
+      }
+    };
 
-		fetchArticle().then();
-		fetchChats().then();
+    fetchArticle().then();
+    fetchChats().then();
+  }, [articleId, setChatData]);
 
-	}, [articleId, setChatData]);
+  const handleNewChat = async () => {
+    // create new chat
+    // Ad_Id
+    // receiver_user_id = ad creator
+    // sender_user_id => loggedin user
+    // return a chatId
 
-	const handleNewChat = async () => {
+    const sender_user_id = userData._id; // Loggedin User
+    const receiver_user_id = article.user_id; // Ad Creator
 
-		// create new chat
-		// Ad_Id
-		// receiver_user_id = ad creator
-		// sender_user_id => loggedin user
-		// return a chatId
+    console.log(sender_user_id, receiver_user_id);
 
+    try {
+      const response = await axios.post(
+        'http://localhost:8000/chats/',
+        {
+          participants: [sender_user_id, receiver_user_id],
+          ad_id: article._id,
+        },
+        { withCredentials: true }
+      );
 
-		const sender_user_id = userData._id; 			// Loggedin User
-		const receiver_user_id = article.user_id; // Ad Creator
+      console.log(response);
+      //setChatData(prevChatData => [...prevChatData, response.data]);
+    } catch (error) {
+      console.error('Error starting new chat:', error);
+    }
 
-		console.log(sender_user_id, receiver_user_id);
+    console.log(chatData);
 
-		try {
-			const response = await axios.post('http://localhost:8000/chats/',
-				{ participants: [sender_user_id, receiver_user_id] },
-				{ ad_id: articleId },
-				{ withCredentials: true }
-			);
+    //navigate(`/singlechat/${chatData[0]._id}`);
+  };
 
-			//setChatData(prevChatData => [...prevChatData, response.data]);
-		}
-		catch (error) {
-			console.error('Error starting new chat:', error);
-		}
+  console.log(isLoggedIn);
 
-		console.log(chatData);
-
-		//navigate(`/singlechat/${chatData[0]._id}`);
-	}
-
-	return (
-		<div>
-			<h1>Single View Ad</h1>
-			<button className="btn-sm btn-red block mb-2" onClick={handleNewChat}>New Chat</button>
-			{/*<button className="btn-sm btn-red block mb-2">Open Chat</button> */}
-		</div>
-	);
-}
+  return (
+    <div>
+      <h1>Single View Ad</h1>
+      <button className='btn-sm btn-red block mb-2' onClick={handleNewChat}>
+        New Chat
+      </button>
+      {/*<button className="btn-sm btn-red block mb-2">Open Chat</button> */}
+    </div>
+  );
+};
 export default SingleViewAd;
--- server/controller/chats.js
+++ server/controller/chats.js
@@ -3,83 +3,103 @@ import ErrorResponse from '../utils/ErrorResponse.js';
 import Chat from '../models/chatsSchema.js';
 import User from '../models/usersSchema.js';
 import Message from '../models/messagesSchema.js';
-import Ads from '../models/adsSchema.js'
+import Ads from '../models/adsSchema.js';
 
 export const createChat = asyncHandler(async (req, res, next) => {
-	const { participants, messages, ad_id } = req.body;
-	const { uid } = req;
+  const { participants, messages, ad_id } = req.body;
+  const { uid } = req;
 
-	console.log('-->', participants);
-	console.log('-->', ad_id); // logged in user id
+  console.log(req.body);
 
-	// Validate that participants is an array and has exactly two members
-	if (!Array.isArray(participants) || participants.length !== 2) {
-		throw new ErrorResponse('Participants must be an array with exactly two user IDs', 400);
-	}
+  // Validate that participants is an array and has exactly two members
+  if (!Array.isArray(participants) || participants.length !== 2) {
+    throw new ErrorResponse(
+      'Participants must be an array with exactly two user IDs',
+      400
+    );
+  }
 
-	// Ensure the logged-in user is the first participant (sender)
-	if (!participants.includes(uid.toString())) {
-		throw new ErrorResponse('Sender must be logged in, you cannot have a conversation. Please login first!', 400);
-	}
+  // Ensure the logged-in user is the first participant (sender)
+  if (!participants.includes(uid.toString())) {
+    throw new ErrorResponse(
+      'Sender must be logged in, you cannot have a conversation. Please login first!',
+      400
+    );
+  }
 
-	// Check if all participants are registered users
-	const users = await User.find({ _id: { $in: participants } });
-	if (users.length !== participants.length) {
-		throw new ErrorResponse('All participants must be registered users', 400);
-	}
+  // Check if all participants are registered users
+  const users = await User.find({ _id: { $in: participants } });
+  if (users.length !== participants.length) {
+    throw new ErrorResponse('All participants must be registered users', 400);
+  }
 
-	//Check if the provided ad_id exists and belongs to one of the participants
-	const adExists = await Ads.exists({ _id: ad_id, user_id: { $in: participants } });
+  //Check if the provided ad_id exists and belongs to one of the participants
+  const adExists = await Ads.exists({
+    _id: ad_id,
+    user_id: { $in: participants },
+  });
 
+  if (!adExists) {
+    throw new ErrorResponse(
+      'Ad does not exist or does not belong to any of the participants',
+      400
+    );
+  }
 
+  // Check if a chat already exists between these participants for the specified ad
+  let chat = await Chat.findOne({
+    participants: { $all: participants },
+    ad_id,
+  });
 
-	if (!adExists) {
-		throw new ErrorResponse('Ad does not exist or does not belong to any of the participants', 400);
-	}
-
-	// Check if a chat already exists between these participants for the specified ad
-	let chat = await Chat.findOne({ participants: { $all: participants }, ad_id });
-
-	// If chat exists, update it with the new messages
-	if (chat) {
-		const newMessages = messages.map(msg => ({
-			chat: chat._id,
-			sender_id: uid,
-			message: msg,
-			ad_id: ad_id // Include ad_id in each message
-		}));
-		const createdMessages = await Message.insertMany(newMessages);
-		chat.messages.push(...createdMessages.map(msg => msg._id));
-		chat.updatedAt = Date.now();
-		await chat.save();
-		res.status(200).json(chat);
-	}
-	else {
-		// If chat does not exist, create a new chat and save the messages
-		const newChat = await Chat.create({ participants, ad_id });
-		const newMessages = messages.map(msg => ({
-			chat: newChat._id,
-			sender_id: uid,
-			message: msg,
-			ad_id: ad_id // Include ad_id in each message
-		}));
-		const createdMessages = await Message.insertMany(newMessages);
-		newChat.messages.push(...createdMessages.map(msg => msg._id));
-		await newChat.save();
-		res.status(201).json(newChat);
-	}
+  // If chat exists, update it with the new messages
+  if (chat) {
+    const newMessages = messages.map((msg) => ({
+      chat: chat._id,
+      sender_id: uid,
+      message: msg,
+      ad_id: ad_id, // Include ad_id in each message
+    }));
+    const createdMessages = await Message.insertMany(newMessages);
+    chat.messages.push(...createdMessages.map((msg) => msg._id));
+    chat.updatedAt = Date.now();
+    await chat.save();
+    res.status(200).json(chat);
+  } else {
+    // If chat does not exist, create a new chat and save the messages
+    const newChat = await Chat.create({ participants, ad_id });
+    const newMessages = messages.map((msg) => ({
+      chat: newChat._id,
+      sender_id: uid,
+      message: msg,
+      ad_id: ad_id, // Include ad_id in each message
+    }));
+    const createdMessages = await Message.insertMany(newMessages);
+    newChat.messages.push(...createdMessages.map((msg) => msg._id));
+    await newChat.save();
+    res.status(201).json(newChat);
+  }
 });
 
 // get a chat
 export const getChatbyId = asyncHandler(async (req, res, next) => {
-  const { params: { id }, uid } = req;
+  const {
+    params: { id },
+    uid,
+  } = req;
   const chat = await Chat.findById(id)
     .populate('participants', 'username') // Populate username field for participants
     .populate('messages');
   if (!chat) throw new ErrorResponse('Chat not found', 404);
 
-  const isParticipant = chat.participants.some(participant => participant._id.toString() === uid.toString());
-  if (!isParticipant) throw new ErrorResponse('You do not have permission to view this chat', 403);
+  const isParticipant = chat.participants.some(
+    (participant) => participant._id.toString() === uid.toString()
+  );
+  if (!isParticipant)
+    throw new ErrorResponse(
+      'You do not have permission to view this chat',
+      403
+    );
 
   res.status(200).json(chat);
 });
@@ -89,13 +109,13 @@ export const getAllChatsForUser = asyncHandler(async (req, res, next) => {
   const { uid } = req;
   const chats = await Chat.find({
     participants: uid,
-    deletedFor: { $ne: uid }
-  }).populate('participants', 'username') // Populate username field for participants
+    deletedFor: { $ne: uid },
+  })
+    .populate('participants', 'username') // Populate username field for participants
     .populate('messages');
 
   res.status(200).json(chats);
 });
-
 
 // Delete a conversation - if user is aprticipants - if he is loggedin - chat should only deleted from his account , other participant can have that chat. - user should be able to delete multiple chats at the same time.
 // we don't really need delete single chat because we can do that using delete multiple chats also.
@@ -108,8 +128,11 @@ export const deleteChat = asyncHandler(async (req, res, next) => {
 
   const found = await Chat.findById(id);
   if (!found) throw new ErrorResponse(`Chat ${id} does not exist`, 404);
-  const isParticipant = found.participants.some(participant => participant.toString() === uid.toString());
-  if (!isParticipant) throw new ErrorResponse('You have no permission to delete this chat', 401);
+  const isParticipant = found.participants.some(
+    (participant) => participant.toString() === uid.toString()
+  );
+  if (!isParticipant)
+    throw new ErrorResponse('You have no permission to delete this chat', 401);
 
   if (!found.deletedFor.includes(uid)) {
     found.deletedFor.push(uid);
@@ -123,12 +146,22 @@ export const deleteChats = asyncHandler(async (req, res, next) => {
   const { ids } = req.body;
   const uid = req.uid; // Retrieve uid from the request object
 
-  if (!Array.isArray(ids) || ids.length === 0) throw new ErrorResponse('No chat IDs provided', 400);
+  if (!Array.isArray(ids) || ids.length === 0)
+    throw new ErrorResponse('No chat IDs provided', 400);
   const chats = await Chat.find({ _id: { $in: ids } });
-  const invalidChats = chats.filter(chat => !chat.participants.some(participant => participant.toString() === uid.toString()));
-  if (invalidChats.length > 0) throw new ErrorResponse('You have no permission to delete some of these chats', 401);
+  const invalidChats = chats.filter(
+    (chat) =>
+      !chat.participants.some(
+        (participant) => participant.toString() === uid.toString()
+      )
+  );
+  if (invalidChats.length > 0)
+    throw new ErrorResponse(
+      'You have no permission to delete some of these chats',
+      401
+    );
 
-	await Chat.updateMany(
+  await Chat.updateMany(
     { _id: { $in: ids } },
     { $addToSet: { deletedFor: uid } }
   );
