4b9ead387eb2c136384d45ad7ae3d23d6f3a494d
sharonVko <shar1.v.kori@gmail.com>
2024-07-07 20:26:11 +0200

    tiny changes in articlelist

--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
-import ArticleCard from "./ArticleCard";
 import axios from "axios";
 import { categories } from "../utils/categories";
 import "../components/css/ArticleList.css";
 import { FaSearch } from "react-icons/fa";
+import ArticleCard_02 from "./ArticleCard_02";
 
 const ArticleList = () => {
   const [ads, setAds] = useState([]);
@@ -266,7 +266,7 @@ const ArticleList = () => {
       <div className="flex flex-col items-center w-full">
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
           {currentArticles.map((ad, index) => (
-            <ArticleCard key={index} article={ad} media={media} />
+            <ArticleCard_02 key={index} article={ad} media={media} />
           ))}
         </div>
         {message && <div className="text-red-500 text-sm mb-4">{message}</div>}
