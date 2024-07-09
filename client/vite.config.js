bc6f99643a52131d70a2eb77f226312d795ab14e
Hanno Bielz <bielzhanno@gmail.com>
2024-07-09 21:01:49 +0200

    design tryout

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    //host: "127.0.0.1",
-    host: 'localhost',
+    host: "127.0.0.1",
+    //host: 'localhost',
     port: 5173,
   },
 });
