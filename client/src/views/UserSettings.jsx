9a523d303206bf6edfa127f836bc889aeb917adc
tdo leipzig <tdo.leipzig@gmail.com>
2024-07-09 16:58:54 +0200

    update user settings

--- client/src/views/UserSettings.jsx
+++ client/src/views/UserSettings.jsx
@@ -1,43 +1,26 @@
 import { useState, useCallback, useEffect } from "react";
 import beispielfotoprofil from "../assets/userlogo.png";
 import { Label } from "flowbite-react";
-import { ReactTags } from "react-tag-autocomplete";
-import { suggestions } from "../components/Categories.jsx";
+
+import { suggestions_cats, suggestions_subcats, suggestions_tags } from "../components/Categories.jsx";
+
+import TagSelect from "../components/TagSelect.jsx";
 import { useAuth } from "../context/AuthProvider.jsx";
 import axios from 'axios';
-
-const MIN_SELECTED_LENGTH = 3;
-const customClassNames = {
-	root: "custom-react-tags",
-	rootIsActive: "custom-is-active",
-	rootIsDisabled: "custom-is-disabled",
-	rootIsInvalid: "custom-is-invalid",
-	label: "custom-react-tags__label",
-	tagList: "custom-react-tags__list",
-	tagListItem: "custom-react-tags__list-item",
-	tag: "custom-react-tags__tag",
-	tagName: "custom-react-tags__tag-name",
-	comboBox: "custom-react-tags__combobox",
-	input: "custom-react-tags__combobox-input",
-	listBox: "custom-react-tags__listbox",
-	option: "custom-react-tags__listbox-option",
-	optionIsActive: "custom-is-active",
-	highlight: "custom-react-tags__listbox-option-highlight",
-};
 
 const UserSettings = () => {
 	const { isLoggedIn, userData } = useAuth();
 	const [profile, setProfile] = useState({
-		// firstname: "",
-		// lastname: "",
-		// email: "",
-		// phone: "",
-		// street: "",
-		// number: "",
+		firstname: "",
+		lastname: "",
+		email: "",
+		phone: "",
+		street: "",
+		number: "",
 		zip: "",
-		// country: "",
-		// name: "",
-		// imageUrl: "",
+		country: "",
+		name: "",
+		imageUrl: "",
 	});
 	const [isEditing, setIsEditing] = useState(false);
 	const [selectedFile, setSelectedFile] = useState(null);
@@ -48,20 +31,19 @@ const UserSettings = () => {
 		if (isLoggedIn && userData) {
 			setProfile((prevProfile) => ({
 				...prevProfile,
-				// firstname: userData.firstname || "",
-				// lastname: userData.lastname || "",
-				// email: userData.email || "",
-				// phone: userData.phone || "",
-				// street: userData.address?.street || "",
-				// number: userData.address?.housenumber || "",
+				firstname: userData.firstname || "",
+				lastname: userData.lastname || "",
+				email: userData.email || "",
+				phone: userData.phone || "",
+				street: userData.address?.street || "",
+				number: userData.address?.housenumber || "",
 				zip: userData.address?.zip || "",
-				// country: userData.address?.city || "",
-				// name: userData.username || "",
-				// imageUrl: userData.profileImage || beispielfotoprofil,
+				country: userData.address?.city || "",
+				name: userData.username || "",
+				imageUrl: userData.profileImage || beispielfotoprofil,
 			}));
 		}
 	}, [isLoggedIn, userData]);
-
 
 	const handleInputChange = (e) => {
 		const { name, value } = e.target;
@@ -70,7 +52,6 @@ const UserSettings = () => {
 			[name]: value,
 		}));
 	};
-
 	const handleImageChange = (event) => {
 		const file = event.target.files[0];
 		if (file) {
@@ -85,7 +66,6 @@ const UserSettings = () => {
 			reader.readAsDataURL(file);
 		}
 	};
-
 	const handleSave = async (e) => {
 		e.preventDefault();
 
@@ -109,110 +89,49 @@ const UserSettings = () => {
 		// console.log("Saved profile:", profile);
 	};
 
-
-	const onAdd = useCallback((newTag) => {
-		setSelected((prevSelected) => [...prevSelected, newTag]);
-	}, []);
-
-	const onDelete = useCallback((index) => {
-		setSelected((prevSelected) => prevSelected.filter((_, i) => i !== index));
-	}, []);
-
-	const isInvalid = selected.length < MIN_SELECTED_LENGTH;
-
 	if (!isLoggedIn) {
 		return <div>Please log in to access your profile.</div>;
 	}
 
-
 	return (
 		<form onSubmit={handleSave}>
-			<div className="container mx-auto grid grid-cols- md:grid-cols-2 bg-teal-600 bg-opacity-80 rounded-lg">
-
-
-				<div className="max-w-sm mx-auto">
-					<div className="text-4xl pb-12 pt-6 text-peach-300 text-center">
-						Profile Settings
-					</div>
-					<div>
-						<img
-							src={profile.imageUrl}
-							alt="Profilbild"
-							className="rounded-full max-w-sm mx-auto h-29 w-40 border-8 border-peach-500 mb-6"
+      <h1 className="mb-6 text-center">Konto-Einstellungen</h1>
+			<div className="container mx-auto grid grid-cols-1 md:grid-cols-2 bg-teal-500 rounded-xl p-4 sm:p-8 md:p-12 mb-12">
+				<div className="mx-auto text-center w-full">
+          <div>
+						<img src={profile.imageUrl} alt="Profilbild" className="rounded-full max-w-sm mx-auto h-29 w-40 ring-8 ring-white/50 mb-6"/>
+						<h2 className="h2 text-peach-300 text-center text-3xl my-8">
+							{profile.firstname} {profile.lastname}
+						</h2>
+						<label htmlFor="profile-image-upload" className="w-[200px] btn-sm btn-red py-2 block mb-3 mx-auto cursor-pointer">
+							Profilbild ändern
+						</label>
+						<input
+							type="file"
+							accept="image/*"
+							onChange={handleImageChange}
+							className="hidden"
+							id="profile-image-upload"
 						/>
-					</div>
-
-					<input
-						type="file"
-						accept="image/*"
-						onChange={handleImageChange}
-						className="hidden"
-						id="profile-image-upload"
-					/>
-					<label
-						htmlFor="profile-image-upload"
-						className="max-w-sm mx-auto btn-sm btn-red text-lemon-500 py-2 mb-4"
-					>
-						Change your Pic
-					</label>
-
-					<h1 className="h2 text-peach-300 text-center text-3xl font-bold mt-8">
-						{profile.firstname} {profile.lastname}
-					</h1>
-
-					<div className="max-w-sm mx-auto">
-						<button className="bg-green-700 text-white rounded-lg border-peach-300 p-3 w-full mb-9 mt-9">
-							Change your Password
+						<button className="w-[200px] btn-sm btn-red py-2 block mx-auto">
+							Passwort ändern
 						</button>
-					</div>
+          </div>
 
-					<div className="mb-2 block">
-						<Label
-							className="text-2xl text-peach-300"
-							htmlFor="categories"
-							value="Select at least three Tags"
-						/>
-					</div>
-					<select
-						id="tag"
-						className="bg-red-400 border-peach-300 text-peach-300 rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-8"
-					>
-						<option className="bg-peach-300 text-black hover:red-100">
-							Shopping
-						</option>
-						<option className="bg-peach-300 focus:ring-orange-500 text-black">
-							Household
-						</option>
-						<option className="bg-peach-300 text-black">Garden</option>
-						<option className="bg-peach-300 text-black">Sport</option>
-					</select>
-					<select
-						id="tag"
-						className="bg-red-400 border-peach-300 text-peach-300 rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-8"
-					>
-						<option className="bg-peach-300 text-black hover-red-100">
-							Shopping
-						</option>
-						<option className="bg-peach-300 focus-ring-orange-500 text-black">
-							Household
-						</option>
-						<option className="bg-peach-300 text-black">Garden</option>
-						<option className="bg-peach-300 text-black">Sport</option>
-					</select>
-					<select
-						id="tag"
-						className="bg-red-400 border-peach-300 text-peach-300 rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-8"
-					>
-						<option className="bg-peach-300 text-black hover:red-100">
-							Shopping
-						</option>
-						<option className="bg-peach-300 focus:ring-orange-500 text-black">
-							Household
-						</option>
-						<option className="bg-peach-300 text-black">Garden</option>
-						<option className="bg-peach-300 text-black">Sport</option>
-					</select>
-				</div>
+          <h2 className="h1 mt-12 text-peach-300 ">Sucheinstellungen</h2>
+          <div className="w-full text-left">
+            <p className="text-2xl text-green-200 mb-4 mt-8 text-center">Mindestens 3 Kategorien auswählen</p>
+            <TagSelect suggestions={suggestions_cats}  />
+          </div>
+          <div className="w-full text-left">
+            <p className="text-2xl text-green-200 mb-4 mt-8 text-center">Mindestens 3 Sub-Kategorien auswählen</p>
+						<TagSelect suggestions={suggestions_subcats}  />
+          </div>
+          <div className="w-full text-left">
+            <p className="text-2xl text-green-200 mb-4 mt-8 text-center">Mindestens 3 Tags auswählen</p>
+						<TagSelect suggestions={suggestions_tags}  />
+          </div>
+        </div>
 
 				<div className="md:col-span-1 p-12 flex flex-col">
 					<div>
@@ -257,15 +176,12 @@ const UserSettings = () => {
 						))}
 
 						{isEditing && (
-							<button type="submit"
-											className="btn-sm btn-red text-lemon-500 px-4 py-2 rounded block mt-4"
-							>
+							<button type="submit" className="btn-sm btn-red text-lemon-500 px-4 py-2 rounded block mt-4">
 								Save
 							</button>
 						)}
 					</div>
 				</div>
-
 			</div>
 		</form>
 	);
