1d3678129628a7cb9199bc7603d6f0de3e5fa969
tdo leipzig <tdo.leipzig@gmail.com>
2024-07-10 11:40:02 +0200

    minor change

--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -269,9 +269,9 @@ const ArticleList = () => {
 
       <div className="flex flex-col items-center w-full">
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
-          {currentArticles.map((ad, index) => (
+          {currentArticles.map((ad, i) => (
             <>
-              <ArticleCard_02 key={index} article={ad} media={media} />
+              <ArticleCard_02 key={i} article={ad} media={media} />
             </>
           ))}
         </div>
