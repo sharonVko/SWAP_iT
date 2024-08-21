5902690c21ef6889c43aec32f5edd36430d14ee3
tdo leipzig <tdo.leipzig@gmail.com>
2024-08-21 14:32:37 +0200

    add context provider for storing vars

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { ContextProvider } from "./context/ContextProvider.jsx";
 import { AuthProvider } from './context/AuthProvider.jsx';
 import { ChatContext } from './context/ChatContext.jsx';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<AuthProvider>
-		<ChatContext>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</ChatContext>
-	</AuthProvider>
+	<ContextProvider>
+		<AuthProvider>
+			<ChatContext>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ChatContext>
+		</AuthProvider>
+	</ContextProvider>
 )
