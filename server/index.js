7df64291507ab8ad62644129576f8cb90b52530e
tdo leipzig <tdo.leipzig@gmail.com>
2024-08-30 11:22:01 +0200

    add custom environment vars for express server

--- server/index.js
+++ server/index.js
@@ -1,72 +1,61 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 import './db/dbConnect.js';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './middleware/ErrorHandler.js';
 import cors from 'cors';
-
 import usersRouter from './routes/usersRouter.js';
 import adsRouter from './routes/adsRouter.js';
 import mediaRouter from './routes/mediaRouter.js';
 import messageRouter from './routes/messageRouter.js';
 import chatRouter from './routes/chatRouter.js'
-
-
-import http from 'http';
-import { Server } from 'socket.io';
 import authMiddleware from './middleware/authSocket.js';
 
-
 const app = express();
-const PORT = 8000;
-
-const server = http.createServer(app); // Creating an HTTP server using the Express application.
+const server= http.createServer(app); // Creating an HTTP server using the Express application.
 
 // Creating a Socket.IO server attached to the HTTP server with CORS configuration.
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ['GET', 'POST']
   }
 });
 
-io.use(authMiddleware); // Middleware to authenticate Socket.IO connections.
+// Middleware to authenticate Socket.IO connections.
+io.use(authMiddleware);
 
 // Event listener for new socket connections
-io.on('connection', (socket) =>
-{
+io.on('connection', (socket) => {
   console.log('a user connected');
 
   // 'joinConversation': Joins the user to a specific conversation room.
-  socket.on('joinConversation', (conversationId) =>
-  {
+  socket.on('joinConversation', (conversationId) => {
     socket.join(conversationId);
   });
 
   // 'sendMessage': Broadcasts a new message to all users in the conversation room.
-  socket.on('sendMessage', (message) =>
-  {
+  socket.on('sendMessage', (message) => {
     io.to(message.chatId).emit('newMessage', message);
   });
 
   // 'disconnect': Logs when a user disconnects.
-  socket.on('disconnect', () =>
-  {
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
 // Middleware to pass the io instance to the request object
-app.use((req, res, next) =>
-{
+app.use((req, res, next) => {
   req.io = io;
   next();
 });
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
 app.use(cookieParser()); // Add cookie-parser middleware
 app.use(errorHandler);
-
 
 // ROUTES
 app.use('/users', usersRouter);
@@ -79,6 +68,6 @@ app.use('/message', messageRouter)
 
 app.use('/media', mediaRouter);
 app.use(errorHandler);
-server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+server.listen(process.env.PORT, () => console.log(`Server is running on PORT: ${process.env.PORT}`));
 
-io.listen(9000);
+io.listen(process.env.IO_PORT);
