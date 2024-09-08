5fa6ee91f3a43d4812ee9b3119c4f69ed4919cbf
tdo leipzig <tdo.leipzig@gmail.com>
2024-09-08 10:12:23 +0200

    minor style changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,90 +28,93 @@ function Navbar({ onToggleNav, onClose }) {
 		}
 	};
 
-	return (
-		<div className="h-24 sm:h-36 lg:h-40 relative flex flex-col justify-center">
-			<div className="bg-teal-500 relative z-400 h-14 sm:h-20 lg:h-24"></div>
+  return (
+    <div className="h-24 sm:h-36 lg:h-40 relative flex flex-col justify-center">
+      <div className="bg-teal-500 relative z-400 h-14 sm:h-20 lg:h-24"></div>
 
-			<div className="absolute top-0 left-0 h-full w-full z-500">
-				<div className="relative px-4">
-					<div className="container mx-auto py-8 flex items-center h-24 sm:h-36 lg:h-40">
-						<button
-							className="text-3xl text-peach-300 flex-1"
-							onClick={onToggleNav}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="1em"
-								height="1em"
-								viewBox="0 0 24 24"
-							>
-								<path
-									fill="none"
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeWidth="2"
-									d="M3 6h18M3 12h18M3 18h18"
-								/>
-							</svg>
-						</button>
+      <div className="absolute top-0 left-0 h-full w-full z-500">
+        <div className="relative px-4">
+          <div className="container mx-auto py-8 flex items-center h-24 sm:h-36 lg:h-40">
+            <button
+              className="text-3xl text-peach-300 hover:text-peach-600 flex-1"
+              onClick={onToggleNav}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  d="M3 6h18M3 12h18M3 18h18"
+                />
+              </svg>
+            </button>
 
-						<div className="w-52">
-							<img
-								src={logo}
-								alt="SwapIt Logo"
-								className="mx-auto w-20 h-20 sm:w-32 sm:h-32 lg:w-36 lg:h-36 drop-shadow-logo cursor-pointer"
-								onClick={() => goTo("/")}
-							/>
-						</div>
+            <div className="w-52 hover:scale-[1.1] transition duration-500 hover:duration-500 ease-in-out">
+              <img
+                src={logo}
+                alt="SwapIt Logo"
+                className="mx-auto w-20 h-20 sm:w-32 sm:h-32 lg:w-36 lg:h-36 drop-shadow-logo cursor-pointer"
+                onClick={() => goTo("/")}
+              />
+            </div>
 
-						<div className="flex-1 flex justify-end gap-4 md:gap-8">
-							<button className="text-2xl text-peach-300">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="1em"
-									height="1em"
-									viewBox="0 0 24 24"
-									onClick={() => goTo("/ads")}
-								>
-									<path
-										fill="none"
-										stroke="currentColor"
-										strokeLinecap="round"
-										strokeWidth="2"
-										d="m21 21l-4.486-4.494M19 10.5a8.5 8.5 0 1 1-17 0a8.5 8.5 0 0 1 17 0Z"
-									/>
-								</svg>
-							</button>
+            <div className="flex-1 flex justify-end gap-4 md:gap-8">
+              <button className="text-2xl text-peach-300 hover:text-peach-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  onClick={() => goTo("/ads")}
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeWidth="2"
+                    d="m21 21l-4.486-4.494M19 10.5a8.5 8.5 0 1 1-17 0a8.5 8.5 0 0 1 17 0Z"
+                  />
+                </svg>
+              </button>
 
-							{isLoggedIn && (
-								<button className="navbar-profile">
-									<img
-										src={userData.profileimage || beispielfotoprofil}
-										alt="Profilbild"
-										className="rounded-full h-10 w-10"
-										onClick={() => goTo("/profile/settings")}
-									/>
-								</button>
-							)}
+              {isLoggedIn && (
+                <button className="navbar-profile">
+                  <img
+                    src={userData.profileimage || beispielfotoprofil}
+                    alt="Profilbild"
+                    className="rounded-full h-10 w-10 hover:scale-110"
+                    onClick={() => goTo("/profile/settings")}
+                  />
+                </button>
+              )}
 
-							{isLoggedIn ? (
-								<button className="btn-sm btn-red" onClick={handleLogout}>
-									Logout
-								</button>
-							) : (
-								<button
-									className="btn-sm btn-red"
-									onClick={() => goTo("/login")}
-								>
-									Login
-								</button>
-							)}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+              {isLoggedIn ? (
+                <button
+                  className="btn-sm btn-red hover:bg-red-500"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              ) : (
+                <button
+                  className="btn-sm btn-red hover:bg-red-500"
+                  onClick={() => goTo("/login")}
+                >
+                  Login
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
